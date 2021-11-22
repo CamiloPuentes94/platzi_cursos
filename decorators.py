ceff53cdcdf1f5e8708db9afebeed223fd72ecbb
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2021-11-21 22:27:41 -0500

    Se realizan modificaciones para que los decoradores sirvan sin restriccion de parametros

--- decorators.py
+++ decorators.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
 def execution_time(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         initial_time = datetime.now()
-        func()
+        func(*args, **kwargs)
         final_time = datetime.now()
         time_elapsed = final_time -initial_time
         print("Pasaron " + str(time_elapsed.total_seconds()) + " Segundos")
@@ -15,4 +15,14 @@ def random_func():
         pass
 
 
+@execution_time
+def suma(a: int, b: int) -> int:
+    return a + b
+
+@execution_time
+def saludo(nombre="Cesar"):
+    print("Hola" + nombre)
+
 random_func()
+suma(5, 5)
+saludo()
