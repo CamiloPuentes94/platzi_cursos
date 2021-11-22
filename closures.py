26f29fa370ad41a390b3a9a67349509f49107b93
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2021-11-21 22:00:38 -0500

    Se realiza programa para realizar closures con una repeticion de hola 5 veces

--- closures.py
+++ closures.py
@@ -2,4 +2,15 @@
 #  Facundo 2 -> FacundoFacundo
 # Platzi 4 -> PlatziPlatziPlatziPlatzi
 
- 
+def make_repeater_of(n):
+    def repeater(string):
+        assert type(string) == str, "Solo puedes utilizar cadenas"
+        return string * n
+    return repeater
+
+def run():
+    repeat5 = make_repeater_of(5)
+    print(repeat5("Hola"))
+
+if __name__ == '__main__':
+    run()
