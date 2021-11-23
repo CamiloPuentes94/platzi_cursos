cb3ce55d22817d0a9694eb067c7c85d1e291a1b1
Juan Camilo Puentes Sandoval <jcpsandoval94@gmail.com>
2021-11-23 15:21:55 -0500

    Se realizan comentarios a el codigo y se adiciona la intersectacion de los sets

--- operaciones_sets.py
+++ operaciones_sets.py
@@ -1,5 +1,8 @@
 my_set1 = {3, 4, 5}
 my_set2 = {5, 6, 7}
 
-my_set_union = my_set1 | my_set2
+my_set_union = my_set1 | my_set2 # con este operador podemos unir dos sets
 print(my_set_union)
+
+my_set_intersectacion = my_set1 & my_set2 # con este operador podemos intersectar dos sets
+print(my_set_intersectacion)
