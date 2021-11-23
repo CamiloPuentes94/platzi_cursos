dfdfd4bb418d3ff4631a3cda2e331c0d9cfc7d58
Juan Camilo Puentes Sandoval <jcpsandoval94@gmail.com>
2021-11-23 15:27:34 -0500

    Se agreaga el operador diferencia simetrica

--- operaciones_sets.py
+++ operaciones_sets.py
@@ -1,3 +1,5 @@
+# Los sirven o se utilizan bastante para hacer join o inner join para las bases de datos
+
 my_set1 = {3, 4, 5}
 my_set2 = {5, 6, 7}
 
@@ -13,3 +15,7 @@ print(my_set_diferencia1)
 
 my_set_diferencia2 = my_set2 - my_set1
 print(my_set_diferencia2)
+
+# diferencia simetrica -- se queda con los valores que no se comparten los sets
+my_set_diff_simetrica = my_set1 ^ my_set2
+print(my_set_diff_simetrica)
